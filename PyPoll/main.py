f5b4d64365ce227bfd056d33a1d791d28a45f72b
Perry the Platypus <jgranger2159@gmail.com>
2018-09-12 16:07:15 -0600

    experimenting a bit

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -9,6 +9,7 @@ file1 = os.path.join("election_data.csv")
 count = 0
 candidates = []
 vote_count = 0
+complete = {}
 
 # Open csv
 with open(file1, newline='') as csvfile:
@@ -25,20 +26,19 @@ with open(file1, newline='') as csvfile:
         candidate = row[2]
         if candidate not in candidates:
             candidates.append([candidate])
-        
-# Print stuff
-print("Election Results")
-print("-"*20)
-print("Total Votes: " + count)
 
 # Count votes/% for each candidate
 with open(file1, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
     for x in range(len(candidates)):
-        name = candidates[x]
         for row in csvreader:
-            if name = row[2]:
+            if candidates[x] = row[2]:
                 vote_count = vote_count + row[1]
         percent = vote_count / count * 100
-        print(x + " " + vote_count + " %" + percent)
+        complete.append(x : vote_count, percent)
+
+# Print stuff
+print("Election Results")
+print("-"*20)
+print("Total Votes: " + count)
