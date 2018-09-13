027a36f6aaebaeff7101e08d1f7b90fc95f11a48
Perry the Platypus <jgranger2159@gmail.com>
2018-09-12 18:31:29 -0600

    just needs debugging

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -25,7 +25,7 @@ with open(file1, newline='') as csvfile:
         # Generate list of candidates
         candidate = row[2]
         if candidate not in candidates:
-            candidates.append([candidate])
+            candidates.append(candidate)
 
 # Count votes/% for each candidate
 with open(file1, newline='') as csvfile:
@@ -33,12 +33,36 @@ with open(file1, newline='') as csvfile:
     csv_header = next(csvreader)
     for x in range(len(candidates)):
         for row in csvreader:
-            if candidates[x] = row[2]:
+            if candidates[x] == row[2]:
                 vote_count = vote_count + row[1]
         percent = vote_count / count * 100
-        complete.append(x : vote_count, percent)
+        complete[x] = vote_count, percent
 
 # Print stuff
 print("Election Results")
 print("-"*20)
 print("Total Votes: " + count)
+print("-"*20)
+for name in complete:
+    print (name)
+    for value in complete[name]:
+        print (value,':',complete[name][value])
+winning_votes = 0
+for k, v in dict.items():
+    greatest_k = k
+    greatest_v = 0
+    if v > greatest_v:
+        greatest_v = v
+        greatest_k = k
+print(greatest_k + ": " + greatest_v)
+
+# Output to file
+f = open("Election_Data.txt", "x")
+f.write("Election Results")
+f.write("-"*20)
+f.write("Total Votes: " + count)
+f.write("-"*20)
+for name in complete:
+    f.write(name)
+    for value in complete[name]:
+        f.write(value,':',complete[name][value])
